51b0b15c480c0d0b02ad5a8daa14b72e9d731faf
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-09-28 14:01:10 +0300

    Controller example

---
+++ Monopoly/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Monopoly.Filters.ActionFilters;
+
+namespace Monopoly.Controllers
+{
+    [ApiController]
+    [ModelValidation]
+    [ServiceFilter(typeof(GuessNumberExceptionsFilterAttribute))]
+    public class IdentityController : ControllerBase
+    {
+
+    }
+}
