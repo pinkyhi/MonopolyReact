f20f79182320f1e7273b0f421f0269dafb09fe8a
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-09-29 11:48:39 +0300

    Settings table

---
+++ Monopoly.DAL/Entities/GameSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly.DAL.Entities
+{
+    public class GameSettings
+    {
+        public int Id { get; set; }
+
+        public int StartMoney { get; set; }
+
+        public int ImprisonmentDuration { get; set; }
+
+        public int JailbreakPrice { get; set; }
+
+        public int ImprisonmentTurnsCount { get; set; }
+
+        public int HousesCount { get; set; }
+
+        public int HotelsCount { get; set; }
+
+        public int RightStartHit { get; set; }
+
+        public int? DoublesCountsForArrest { get; set; }
+
+        public bool DoubleRepeatsTurn { get; set; }
+
+        public bool AnytimeExchange { get; set; }
+
+        public bool AnytimeBuilding { get; set; }
+
+        public bool MovementFields { get; set; }
+
+        public bool Auction { get; set; }
+
+        public bool BuildingBalancing { get; set; }
+
+        public bool FullCollateralValue { get; set; }
+
+        public bool DoublePayOnUnbuildedMonopoly { get; set; }
+
+        public bool DoubleBreaksJail { get; set; }
+
+        public bool Casino { get; set; }
+    }
+}
