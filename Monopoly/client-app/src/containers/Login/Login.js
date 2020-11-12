886f7a1518f56d3375a22c946e817f434f1aceb2
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-11-12 14:07:27 +0200

    LoginPage

--- Monopoly/client-app/src/containers/Login/Login.js
+++ Monopoly/client-app/src/containers/Login/Login.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react'
 import classes from './Login.css'
+import { Link } from 'react-router-dom'
+
 export default class Login extends Component{
     render(){
         return(
             <div className={classes.Login}>
-                <h1 className="headerWhite">Login</h1>
+                <h1>Login</h1>
+                <form>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                    <p className="forgot-password text-right">
+                        Forgot <Link to="#">password?</Link>
+                    </p>
+                </form>
             </div>
         )
     }
--- Monopoly/client-app/src/hoc/Navigation/Navigation.js
+++ Monopoly/client-app/src/hoc/Navigation/Navigation.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react'
 import { Navbar,Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Logo from './../../logo.svg'
 
 class Navigation extends Component{
     render(){
         return(
             <div>
                 <Navbar bg="light" expand="lg">
-                <Navbar.Brand to="/">Monopoly</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">
+                    <img
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt=""
+                    />
+                    Monopoly
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link as={Link} to="/">Lobby</Nav.Link>
-                    <Nav.Link as={Link} to="/register">Register</Nav.Link>
-                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                    <Nav.Item>Username</Nav.Item>
-                    <Nav.Link>Logout</Nav.Link>
+                        <Nav.Link as={Link} to="/">Lobby</Nav.Link>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link disabled>Username</Nav.Link>
+                        <Nav.Link>Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
