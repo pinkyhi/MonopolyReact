841a07d27eee6a17b1109d737dc4e5707b8ff591
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-11-12 17:07:22 +0200

    Sign up

--- Monopoly/client-app/src/containers/Login/Login.js
+++ Monopoly/client-app/src/containers/Login/Login.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import classes from './Login.css'
-import { Link } from 'react-router-dom'
 
 export default class Login extends Component{
     render(){
@@ -21,14 +20,10 @@ export default class Login extends Component{
                     <div className="form-group">
                         <div className="custom-control custom-checkbox">
                             <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                         </div>
                     </div>
 
                     <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-                    <p className="forgot-password text-right">
-                        Forgot <Link to="#">password?</Link>
-                    </p>
                 </form>
             </div>
         )
--- Monopoly/client-app/src/containers/Register/Register.js
+++ Monopoly/client-app/src/containers/Register/Register.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react'
+import classes from './Register.css'
+import { Link } from 'react-router-dom'
 
 export default class Register extends Component{
     render(){
         return(
-            <div>
+            <div className={classes.Register}> 
                 <h1>Register</h1>
+                <form>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" className="form-control" placeholder="Username" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                <p className="forgot-password text-right">
+                    Already registered <Link to="/login">log in?</Link>
+                </p>
+            </form>
             </div>
         )
     }
